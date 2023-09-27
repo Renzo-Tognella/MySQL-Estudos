ccbcb475c2da249bfd991dca0fde6542213d90ab
Renzo Tognella <122568128+Renzo-Tognella@users.noreply.github.com>
2023-09-26 22:54:32 -0300

    Update operadores.sql

--- operadores.sql
+++ operadores.sql
@@ -42,3 +42,26 @@ and ano = '2014';
 select * from senso
 where populacao <= 10000
 and ano = '2014';
+
+-- usando operadores matematicos 
+
+select 1+3 as retorno; 
+
+select 2*3 as retorno;
+
+select 1-3 as retorno;
+
+select 4/2 as retorno;
+
+select 40%7 as retorno;
+
+-- combinando operadores matematicos 
+
+select ((1+4) * (3*2))/5 as retorno;
+
+-- script projetando acrescimo de 10% na população 
+
+select nome_mun, populacao, populacao * 1.10 as projecaoPOP, populacao * 0.10 as aumento
+from senso 
+where ano = '2014';
+
